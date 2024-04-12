95ae9d0b2701ca72d613e1a2b6658cadf9678b58
Vijaya K Eyunni <evkumar71@yahoo.com>
2024-04-11 19:16:34 -0700

    scripts to deploy on AWS

--- sh/add_step.sh
+++ sh/add_step.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 REGION="ca-central-1"
-CLUSTER_ID="j-3IECOL9TNB48K"
-NAME="hello-world"
+CLUSTER_ID="j-ZWJM9UTGNOQ2"
+NAME="Metrics-example"
 BUCKET_NAME="s3://vijay-pyspark-etl"
 PY_FILES="${BUCKET_NAME}/application.zip"
 FILES="${BUCKET_NAME}/config/conf-aws.json"
-MAIN="${BUCKET_NAME}/src/helloworld.py"
+MAIN="${BUCKET_NAME}/src/metrics.py"
 ARGS="conf-aws.json"
 
 aws emr add-steps --region ${REGION} --cluster-id ${CLUSTER_ID} \
