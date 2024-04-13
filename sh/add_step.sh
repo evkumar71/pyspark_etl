a3c3efa9917a9d2cc46a38670bb8b53881f720d6
Vijaya K Eyunni <evkumar71@yahoo.com>
2024-04-12 22:19:21 -0700

    changes to deploy on aws-emr cluster

--- sh/add_step.sh
+++ sh/add_step.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 REGION="ca-central-1"
-CLUSTER_ID="j-ZWJM9UTGNOQ2"
+CLUSTER_ID="j-2IFKPBME81FFM"
 NAME="Metrics-example"
 BUCKET_NAME="s3://vijay-pyspark-etl"
 PY_FILES="${BUCKET_NAME}/application.zip"
 FILES="${BUCKET_NAME}/config/conf-aws.json"
-MAIN="${BUCKET_NAME}/src/metrics.py"
+MAIN="${BUCKET_NAME}/src/prepare.py"
 ARGS="conf-aws.json"
 
 aws emr add-steps --region ${REGION} --cluster-id ${CLUSTER_ID} \
